dacdfb352398d076bb48165572c716e6ec8f6af5
Partyka <O733503@NAEAST.AD.JPMORGANCHASE.com>
2021-03-01 12:19:48 -0600

    create contact form

--- fedexapp/forms.py
+++ fedexapp/forms.py
@@ -2,3 +2,9 @@ from django import forms
 
 class NameForm(forms.Form):
     shipper_name = forms.CharField(label='shipper_name', max_length=35)
+
+class ContactForm(forms.Form):
+    subject = forms.CharField(max_length=100)
+    message = forms.CharField(widget=forms.Textarea, default='foo')
+    sender = forms.EmailField()
+    cc_myself = forms.BooleanField(required=False)
--- fedexapp/views.py
+++ fedexapp/views.py
@@ -1,21 +1,21 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from .forms import NameForm
+from .forms import ContactForm
 
 # Create your views here.
 def ship(request):
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
-        form = NameForm(request.POST)
+        form = ContactForm(request.POST)
         if form.is_valid():
             # process the data in form.cleaned_data as required
             # redirect to a new URL:
             return HttpResponseRedirect('/shipped/')
     # if a GET (or any other method), create a blank form
     else:
-        form = NameForm()
+        form = ContactForm()
 
     return render(request, 'fedexapp/ship.html', {'form': form})
 
